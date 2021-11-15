d0c1b5f72912998397989cc6b10d87518b002fa8
marie3003 <marie.viktoria.becker@gmail.com>
2021-11-15 17:15:41 +0100

    homework 5

--- tests/test_proteins.py
+++ tests/test_proteins.py
@@ -23,5 +23,17 @@ def test_protein_sequence():
     sequence_t = p_A.sequence
     assert sequence_t == "AAA"
 
-def test_plot_hist():
-    assert
+def test_find_metric_values():
+    p_AR = proteins.basic.Protein('Protein AR', "AR")
+    metric_v = p_AR.find_metric_values()
+    assert metric_v == [1.8, -4.5]
+
+def test_calculate_sliding_window():
+    p_AR = proteins.basic.Protein('Protein AR', "ARARA")
+    mean_v = p_AR.calculate_sliding_window()
+    assert mean_v == [1.8, -1.35, -0.30000000000000004, -1.35, -0.7200000000000001]
+
+def test_create_positions():
+    p_AR = proteins.basic.Protein('Protein AR', "ARARA")
+    positions = p_AR.create_positions()
+    assert positions == [0,1,2,3,4]
